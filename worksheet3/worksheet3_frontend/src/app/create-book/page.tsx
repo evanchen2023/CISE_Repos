2592297cce7129c559259444c89edb226f723389
evanchen2022 <jrp9421@autuni.ac.nz>
2024-08-06 09:37:25 +1200

    updated worksheet3_frontend

---
+++ worksheet3/worksheet3_frontend/src/app/create-book/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import CreateBookComponent from "@/components/CreateBook";
+
+export default function CreateBook() {
+return (
+        <main>
+            <CreateBookComponent />
+        </main>
+    );
+}
---
+++ worksheet3/worksheet3_frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import ShowBookList from "@/components/ShowBookList";
+
+export default function Home() {
+  return (
+    <main>
+      <ShowBookList />
+    </main>
+  );
+}
+
