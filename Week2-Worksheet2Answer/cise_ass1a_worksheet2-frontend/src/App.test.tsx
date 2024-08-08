f1cd2d1fd7255c249b60cb12269dd6a9e7cc0c2f
evanchen2022 <jrp9421@autuni.ac.nz>
2024-08-08 21:28:21 +1200

    adjust the version

--- Week2-Worksheet2Answer/cise_ass1a_worksheet2-frontend/src/App.test.tsx
+++ Week2-Worksheet2Answer/cise_ass1a_worksheet2-frontend/src/App.test.tsx
@@ -19,7 +19,7 @@ it('renders without crashing', () => {
 });
 
 describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {expect(2 + 2).toBe(4);
+  it('knows that 2 and 2 make 4', () => {expect(2 + 2).toBe(5);
   });
 });
 
