6315b3ecf47b1173299b95466e21b4096b8e8445
evanchen2022 <jrp9421@autuni.ac.nz>
2024-08-12 16:35:29 +1200

    Updated App.tsx

--- Week2-Worksheet2Answer/cise_ass1a_worksheet2-frontend/src/App.tsx
+++ Week2-Worksheet2Answer/cise_ass1a_worksheet2-frontend/src/App.tsx
@@ -38,7 +38,7 @@ class App extends Component<{}, State> {
       <div>
         <p>Count: {this.state.count}</p>
         <button className="increment" onClick={this.increment}>
-          Increment count
+          Increment count, press here!!!
         </button>
       </div>
     );
